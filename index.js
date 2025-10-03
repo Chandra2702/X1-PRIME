24411cebd4e4c8356e6c13c1bc44fa808d8d40c8
Chandra2702 <99614677+Chandra2702@users.noreply.github.com>
2025-10-03 21:00:56 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+{
+  "name": "multi-tools-unlock-x1",
+  "version": "1.0.0",
+  "description": "Multi UNLOCK Tools - X1 Prime (Node.js port of batch script)",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "inquirer": "^9.2.7",
+    "chalk": "^5.3.0"
+  },
+  "bin": {
+    "multi-unlock": "./index.js"
+  },
+  "author": "Andriy Chandra",
+  "license": "MIT"
+}
